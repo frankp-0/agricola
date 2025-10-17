a07bf1d1934902d403de53b339992c48107cebeb
Frank Ockerman <frank.ockerman@gmail.com>
2025-10-16 21:13:29 -0400

    Add c++ code for reading flare and rfmix

--- src/lagga/__init__.py
+++ src/lagga/__init__.py
@@ -1,3 +1,8 @@
+from lagga._cpp import read_flare, read_rfmix
 from .data import GenoAncestryDataset
 from .step1 import step1
 from .step2 import step2
+
+
+# def hello() -> str:
+#    return hello_from_bin()
