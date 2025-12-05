43e910d80fc2ad8e25725300e6186e3571a79270
Frank Ockerman <frank.ockerman@gmail.com>
2025-12-05 09:21:43 -0500

    Refactor and add comments/docstrings to step 2

--- src/lagga/step2.py
+++ src/lagga/step2.py
@@ -1,6 +1,6 @@
 import jax
 import jax.numpy as jnp
-from jaxtyping import Array, ArrayLike
+from jaxtyping import Array
 import numpy as np
 from scipy.stats import chi2
 from typing import List
@@ -16,19 +16,29 @@ from jax.scipy.linalg import solve
 
 
 @jax.jit
-def _step2_block_core(
+def _step2_qt_core(
     G: Array,
     L: Array,
     Y: Array,
-    Q_covar: Array,
-    eps=jnp.float32(1e-8),
+    Q: Array,
+    eps: np.float32 = jnp.float32(1e-8),
 ) -> dict[str, Array]:
-    k = G.shape[2]
+    """Estimate coefficients and Wald statistic for quantitative traits
 
-    # Adjust X
+    Args:
+        G: A (N, B, len(ancestries)) jax array of anc-deconvoluted genotypes
+        L: A (N, B, len(ancestries) - 1) jax array of local ancestry
+        Y: A (N, P) jax array of LOCO phenotypes
+        Q: (N, C) jax array. The orthogonal matrix Q in the QR decomposition of the covariates
+        eps: A tolerance term (for non-intervertible design matrix)
+    """
+
+    ## Create predictor array X
     X = jnp.concatenate([G, L], axis=-1)
-    QX = jnp.einsum("nc,nbk->cbk", Q_covar, X)
-    X = X - jnp.einsum("nc,cbk->nbk", Q_covar, QX)
+
+    ## Adjust X for covariates
+    QX = jnp.einsum("nc,nbk->cbk", Q, X)
+    X = X - jnp.einsum("nc,cbk->nbk", Q, QX)
 
     # Fit regression
     XtX = jnp.einsum("nbc,nbd->bcd", X, X)
@@ -36,8 +46,13 @@ def _step2_block_core(
     XtX_inv = jnp.linalg.inv(XtX + eps * I_[None, :, :])
     XtY = jnp.einsum("nbc,np->bcp", X, Y)
     beta_hat = XtX_inv @ XtY
+
+    ## Subset to anc-specific effects (ignore local ancestry terms)
+    k = G.shape[2]
     beta_G = beta_hat[:, :k, :]
-    sig_e = np.sum(Y**2, axis=0) / (Y.shape[0] - X.shape[2] - Q_covar.shape[1])
+
+    ## Calculate Wald statistic and coefficient standard errors
+    sig_e = np.sum(Y**2, axis=0) / (Y.shape[0] - X.shape[2] - Q.shape[1])
     W = jnp.einsum("bkp,bkp->bp", beta_G, solve(XtX_inv[:, :k, :k], beta_G)) / sig_e
     se_beta_G = jnp.sqrt(
         jnp.diagonal(XtX_inv[:, :k, :k], axis1=-2, axis2=-1)[:, :, None]
@@ -51,29 +66,41 @@ def _step2_block_core(
     }
 
 
-def _step2_block(
+def _step2_qt_block(
     dataset: GenoAncestryDataset,
     Y: Array,
-    Q_covar: Array,
+    Q: Array,
     block: np.ndarray,
     min_anc_ac: int = 1,
 ):
-    """Perform step 2 for a block of variants"""
+    """Perform GWAS for quantitative traits for a single block of variants
+
+    Args:
+        dataset: GenoAncestryDataset
+        Y: A (N, P) jax array of LOCO phenotypes
+        Q: (N, C) jax array. The orthogonal matrix Q in the QR decomposition of the covariates
+        block: A (B,) ndarray with indices of variants in the block
+        min_anc_ac: The minimum count of ancestry-deconvoluted alleles to test
+
+    Returns:
+        result_dfs: A list of pandas tables (per-phenotype) with GWAS results
+    """
+
+    ## Query local ancestry and anc-deconvoluted genotypes
     G = dataset.get_lanc_geno(block)
     L = dataset.get_lanc_unphased(block)[:, :, 1:]
-    anc_ac = G.sum(axis=0)
 
-    anc_variant_mask = anc_ac.sum(axis=1) >= min_anc_ac
-
-    res = _step2_block_core(G, L, Y, Q_covar)
-
+    ## Estimate coefficients and Wald statistic
+    res = _step2_qt_core(G, L, Y, Q)
     wald = np.array(res["wald"])
     se = np.array(res["se"])
     beta_hat = np.array(res["beta_hat"])
 
+    ## Calculate p-values
     log10p_overall = chi2.logsf(wald, G.shape[2]) / np.log(10)
     log10p_anc = chi2.logsf((beta_hat / se) ** 2, 1) / np.log(10)
 
+    ## Create array with results
     result_arr = np.concatenate(
         [
             beta_hat,
@@ -84,10 +111,7 @@ def _step2_block(
         axis=1,
     )
 
-    # Variant info + DataFrames
-    block_info = dataset.get_info(block)  # all variants
-    block_info["N"] = Y.shape[0]
-
+    ## Get column names for results
     ancs = dataset.ancestries
     colnames: List[str] = [
         *["beta_" + anc for anc in ancs],
@@ -96,13 +120,18 @@ def _step2_block(
         *["log10p_" + anc for anc in ancs],
     ]
 
-    p = Y.shape[1]
+    ## Get info on variants in block
+    block_info = dataset.get_info(block)  # all variants
+    block_info["N"] = Y.shape[0]
 
-    # Filter for reporting only variants that pass min_anc_ac
+    ## Filter out variants that fail min_anc_ac
+    anc_variant_mask = G.sum(axis=0).sum(axis=1) >= min_anc_ac
     valid_idx = np.array(anc_variant_mask)
     block_info_filtered = block_info[valid_idx]
     result_arr_filtered = result_arr[valid_idx, :, :]
 
+    ## Format results into list of dataframes
+    p = Y.shape[1]
     result_dfs = [
         pd.concat(
             [
@@ -117,29 +146,30 @@ def _step2_block(
     return result_dfs
 
 
-def _step2_dataset(
+def _step2_qt_dataset(
     dataset: GenoAncestryDataset,
     Y_loco: dict[str, np.ndarray],
-    Q_covar: Array,
+    Q: Array,
     out_prefix: str,
     phenotypes: list[str],
     desc: str,
     B: int = 2000,
     variants: Optional[list[str]] = None,
 ):
-    """Perform GWAS for a single dataset
+    """Perform GWAS for quantitative traits for a single dataset
 
     Args:
         dataset: GenoAncestryDataset
         Y_loco: A dict where each value is a (N, P) NumPy array with LOCO residuals from step 1
-        Q_covar: (N, C) jax array. The orthogonal matrix Q in the QR decomposition
-        of the covariates
+        Q: (N, C) jax array. The orthogonal matrix Q in the QR decomposition of the covariates
         out_prefix: Outputs will be written {output_prefix}_{phenotype}.parquet
         phenotypes: A list of phenotype names
         desc: A string describing the dataset, used for tracking progress
         B: The block size (max number of variants to read at once)
+        variants: A list of variant IDs to include in the analysis. If not provided, all variants are used
     """
 
+    ## Get variant indices
     if variants is None:
         idx_variant = np.arange(dataset.pvar.get_variant_ct(), dtype=np.uint32)
     else:
@@ -152,16 +182,16 @@ def _step2_dataset(
             [i for i, x in enumerate(dataset_ids) if x in varset], dtype=np.uint32
         )
 
+    ## Get chromosomes and number of blocks
     chromosomes = [
         dataset.pvar.get_variant_chrom(i).decode("utf8") for i in idx_variant
     ]
-
     chroms = list(set(chromosomes))  # unique chromosomes
-
     n_blocks = sum(
         (len([c for c in chromosomes if c == chrom]) + B - 1) // B for chrom in chroms
     )
 
+    ## Initialize output
     out_paths = [Path(f"{out_prefix}_{pheno}.parquet") for pheno in phenotypes]
     for p in out_paths:
         p.parent.mkdir(parents=True, exist_ok=True)
@@ -169,20 +199,21 @@ def _step2_dataset(
     writers = [None] * len(phenotypes)
     schemas = [None] * len(phenotypes)
 
+    ## Perform step 2 for each chromosome and block
     with tqdm(total=n_blocks, desc=desc, unit="block") as pbar:
         for chrom in chroms:
-            # Get indices for this chromosome
+            ## Get indices for this chromosome
             idx_chrom = np.array(
                 [i for i, c in enumerate(chromosomes) if c == chrom], dtype=np.uint32
             )
-            # Split into blocks
+            ## Split into blocks
             blocks = [
                 idx_variant[idx_chrom[i : i + B]] for i in range(0, len(idx_chrom), B)
             ]
 
             for block in blocks:
-                result_dfs = _step2_block(
-                    dataset, jnp.asarray(Y_loco[chrom]), Q_covar, block
+                result_dfs = _step2_qt_block(
+                    dataset, jnp.asarray(Y_loco[chrom]), Q, block
                 )
                 for i, df in enumerate(result_dfs):
                     table = pa.Table.from_pandas(df, preserve_index=False)
@@ -207,11 +238,11 @@ def step2_qt(
     B: int = 1000,
     variants: Optional[list[str]] = None,
 ):
-    """Run step 2 for each dataset
+    """Run step 2 for quantitative traits
 
     Args:
         datasets: A list of GenoAncestryDataset objects
-        Y: An (N, P) array of outcomes
+        Y: An (N, P) jax array of outcomes
         step1_predictions: A dict with chromosome-specific predictions from step 1.
         The values are (N, P) NumPy arrays
         out_prefixes: A list of prefixes for each dataset. Outputs will be written
@@ -220,12 +251,17 @@ def step2_qt(
         covar: An (N, C) array of covariates. If not provided, defaults to intercept-only covariates
         B: The block size (max number of variants to read at once)
     """
+
+    ## Residualize and standardize phenotypes
     Q, _ = jnp.linalg.qr(X, mode="reduced")
     Y = _stdize(Y - (Q @ (Q.T @ Y)))
 
+    ## Get LOCO predictions
     step1_prs = np.sum(np.stack(list(step1_predictions.values())), axis=0)
     Y_loco = {k: np.asarray(Y) - (step1_prs - v) for k, v in step1_predictions.items()}
+
+    ## Step 2
     for i, ds in enumerate(datasets):
         pgen_path = ds.plink_prefix + ".pgen"
         desc = f"Getting step 2 results for file: {pgen_path}"
-        _step2_dataset(ds, Y_loco, Q, out_prefixes[i], phenotypes, desc, B, variants)
+        _step2_qt_dataset(ds, Y_loco, Q, out_prefixes[i], phenotypes, desc, B, variants)
