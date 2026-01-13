63fb57df332d79d9d968077cbd91757ec9e9d32e
Frank Ockerman <frank.ockerman@gmail.com>
2026-01-13 15:09:01 -0500

    test: add pytest tests for cli

--- TODO.md
+++ TODO.md
@@ -5,5 +5,4 @@
 - [ ] Sphinx documentation
 - [ ] Add option to subset samples in step0,1,2
 - [ ] Check that sample size in datasets matches array input
-- [ ] Tests for CLI
 - [ ] Fix type hints
---
+++ tests/test_cli.py
@@ -0,0 +1,92 @@
+import pytest
+import pandas as pd
+from pathlib import Path
+from typer.testing import CliRunner
+from lagga.cli import app
+import jax
+
+runner = CliRunner()
+
+
+@pytest.fixture
+def toy_data(tmp_path: Path):
+    N = 20
+    P = 4
+    C = 2
+
+    ## Phenotypes
+    pheno_file = tmp_path / "pheno.csv"
+    key0 = jax.random.key(8899134)
+    df_traits = pd.DataFrame(jax.random.normal(key0, shape=(N, P)))
+    df_traits.columns = ["trait" + str(i) for i in range(0, 4)]
+    df_traits.to_csv(pheno_file)
+
+    ## Covariates
+    covar_file = tmp_path / "covar.csv"
+    key1 = jax.random.key(13100)
+    df_covars = pd.DataFrame(jax.random.normal(key1, shape=(N, C)))
+    df_covars.columns = ["covar" + str(i) for i in range(0, 2)]
+    df_covars.to_csv(covar_file)
+
+    return {
+        "pheno_file": str(pheno_file),
+        "covar_file": str(covar_file),
+        "plink_prefix": ",".join(
+            ["tests/data/chr" + str(chr) for chr in range(20, 23)]
+        ),
+        "lanc_file": ",".join(
+            ["tests/data/chr" + str(chr) + ".lanc" for chr in range(20, 23)]
+        ),
+        "step1_prefix": str(tmp_path / "out"),
+        "step2_prefix": ",".join(
+            [str(tmp_path / "chr") + str(chr) for chr in range(20, 23)]
+        ),
+    }
+
+
+def test_step1_toy(toy_data):
+    result = runner.invoke(
+        app,
+        [
+            "step1",
+            "--plink-prefix",
+            toy_data["plink_prefix"],
+            "--lanc-file",
+            toy_data["lanc_file"],
+            "--out-prefix",
+            toy_data["step1_prefix"],
+            "--pheno-file",
+            toy_data["pheno_file"],
+            "--covar-file",
+            toy_data["covar_file"],
+        ],
+    )
+    assert result.exit_code == 0
+
+    out_file = toy_data["step1_prefix"] + ".pkl"
+    assert Path(out_file).exists()
+
+
+def test_step2_toy(toy_data):
+    result = runner.invoke(
+        app,
+        [
+            "step2",
+            "--plink-prefix",
+            toy_data["plink_prefix"],
+            "--lanc-file",
+            toy_data["lanc_file"],
+            "--step1-prefix",
+            "tests/data/step1_pred",
+            "--out-prefix",
+            toy_data["step2_prefix"],
+            "--pheno-file",
+            toy_data["pheno_file"],
+            "--covar-file",
+            toy_data["covar_file"],
+        ],
+    )
+    assert result.exit_code == 0
+
+    file_20 = toy_data["step2_prefix"][0]
+    assert Path(file_20).exists()
