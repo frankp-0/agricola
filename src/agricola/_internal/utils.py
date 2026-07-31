frankp-0/agricola

commit 886d92657884262ec2b951c65206d9a3ca99c960

feat: add option to use double instead of single precision

src/agricola/_internal/utils.py
@@ -104,8 +104,8 @@ def get_geno_lanc_deconv(
     geno = jnp.asarray(dataset.get_geno(indices))
     lanc = jnp.asarray(dataset.get_lanc(indices))
     ancestries = jnp.arange(len(dataset.ancestries))
-    left_haps_mask = (lanc[:, :, 0:1] == ancestries[None, None, :]).astype(np.int32)
-    right_haps_mask = (lanc[:, :, 1:2] == ancestries[None, None, :]).astype(np.int32)
+    left_haps_mask = lanc[:, :, 0:1] == ancestries[None, None, :]
+    right_haps_mask = lanc[:, :, 1:2] == ancestries[None, None, :]
     geno_masked = left_haps_mask * geno[:, :, 0:1] + right_haps_mask * geno[:, :, 1:2]
     lanc_masked = left_haps_mask + right_haps_mask
     return geno_masked, lanc_masked
@@ -126,7 +126,7 @@ def get_geno_deconv(dataset: LancData, indices: NDArray[np.uint32]) -> Array:
     geno = jnp.asarray(dataset.get_geno(indices))
     lanc = jnp.asarray(dataset.get_lanc(indices))
     ancestries = jnp.arange(len(dataset.ancestries))
-    left_haps_mask = (lanc[:, :, 0:1] == ancestries[None, None, :]).astype(np.int32)
-    right_haps_mask = (lanc[:, :, 1:2] == ancestries[None, None, :]).astype(np.int32)
+    left_haps_mask = lanc[:, :, 0:1] == ancestries[None, None, :]
+    right_haps_mask = lanc[:, :, 1:2] == ancestries[None, None, :]
     geno_masked = left_haps_mask * geno[:, :, 0:1] + right_haps_mask * geno[:, :, 1:2]
     return geno_masked

src/agricola/_internal/inputs.py
@@ -43,7 +43,7 @@ def validate_level0_inputs(
 
     ## X
     if X is None:
-        X = jnp.ones((Y.shape[0], 1), dtype=np.float32)
+        X = jnp.ones((Y.shape[0], 1), dtype=float)
     else:
         X = jnp.asarray(X)
         if X.ndim != 2:
@@ -52,7 +52,7 @@ def validate_level0_inputs(
             raise ValueError(
                 f"X.shape[0] must match Y.shape[0], got {X.shape[0]} vs {N}"
             )
-        X = jnp.concatenate([jnp.ones((Y.shape[0], 1), dtype=np.float32), X], axis=1)
+        X = jnp.concatenate([jnp.ones((Y.shape[0], 1), dtype=float), X], axis=1)
     X = stdize(X)
     assert_covar_full_rank(X)
 
@@ -131,7 +131,7 @@ def validate_level1_inputs(
 
     ## X
     if X is None:
-        X = jnp.ones((Y.shape[0], 1), dtype=np.float32)
+        X = jnp.ones((Y.shape[0], 1), dtype=float)
     else:
         X = jnp.asarray(X)
         if X.ndim != 2:
@@ -140,7 +140,7 @@ def validate_level1_inputs(
             raise ValueError(
                 f"X.shape[0] must match Y.shape[0], got {X.shape[0]} vs {N}"
             )
-        X = jnp.concatenate([jnp.ones((Y.shape[0], 1), dtype=np.float32), X], axis=1)
+        X = jnp.concatenate([jnp.ones((Y.shape[0], 1), float), X], axis=1)
     X = stdize(X)
     assert_covar_full_rank(X)
 
@@ -200,7 +200,7 @@ def validate_step2_inputs(
         )
 
     if X is None:
-        X = jnp.ones((Y.shape[0], 1), dtype=np.float32)
+        X = jnp.ones((Y.shape[0], 1), dtype=float)
     else:
         X = jnp.asarray(X)
         if X.ndim != 2:
@@ -209,7 +209,7 @@ def validate_step2_inputs(
             raise ValueError(
                 f"X.shape[0] must match Y.shape[0], got {X.shape[0]} vs {N}"
             )
-        X = jnp.concatenate([jnp.ones((Y.shape[0], 1), dtype=np.float32), X], axis=1)
+        X = jnp.concatenate([jnp.ones((Y.shape[0], 1), dtype=float), X], axis=1)
     X = stdize(X)
     assert_covar_full_rank(X)
 

src/agricola/_internal/level0.py
@@ -173,7 +173,7 @@ def level0(
             ]
             Zs = [
                 np.lib.format.open_memmap(
-                    fnames[i], mode="w+", dtype=np.float32, shape=(N, n_blocks * K)
+                    fnames[i], mode="w+", dtype=float, shape=(N, n_blocks * K)
                 )
                 for i in range(len(phenotypes))
             ]

src/agricola/cli.py
@@ -409,14 +409,25 @@ def step1(
     loocv: bool = typer.Option(
         False, help="Use leave-one-out cross-validation (only for rare binary traits)"
     ),
+    double_precision: bool = typer.Option(
+        False,
+        help=(
+            "Force double precision (default single precision) in JAX. "
+            "This option can be ignored if the environment variable JAX_ENABLE_X64=True is already set."
+        ),
+    ),
 ) -> None:
-    logger.info(_get_options_msg(locals()))
+    import jax
+
+    if double_precision:
+        jax.config.update("jax_enable_x64", True)
 
     import jax.numpy as jnp
-    import jax
     from ._internal.utils import get_cv_mask
     from .step1 import step1
     import numpy as np
+
+    logger.info(_get_options_msg(locals()))
 
     ## Load data
     ancestries_list = _list_from_csv(ancestries)
@@ -554,11 +565,23 @@ def step2(
         False,
         help="Impute quantitative traits in step 2 (must be --no-impute for binary traits)",
     ),
+    double_precision: bool = typer.Option(
+        False,
+        help=(
+            "Force double precision (default single precision) in JAX. "
+            "This option can be ignored if the environment variable JAX_ENABLE_X64=True is already set."
+        ),
+    ),
 ) -> None:
-    logger.info(_get_options_msg(locals()))
+    import jax
+
+    if double_precision:
+        jax.config.update("jax_enable_x64", True)
 
     from .step2 import step2
     import numpy as np
+
+    logger.info(_get_options_msg(locals()))
 
     ## Load data
     ancestries_list = _list_from_csv(ancestries)
@@ -709,11 +732,22 @@ def all_steps(
         False,
         help="Impute quantitative traits in step 2 (must be --no-impute for binary traits)",
     ),
+    double_precision: bool = typer.Option(
+        False,
+        help=(
+            "Force double precision (default single precision) in JAX. "
+            "This option can be ignored if the environment variable JAX_ENABLE_X64=True is already set."
+        ),
+    ),
 ) -> None:
+    import jax
+
+    if double_precision:
+        jax.config.update("jax_enable_x64", True)
+
     logger.info(_get_options_msg(locals()))
 
     import jax.numpy as jnp
-    import jax
     import numpy as np
     from ._internal.utils import get_cv_mask
     from .step1 import step1

src/agricola/step1.py
@@ -11,7 +11,6 @@ steps . The entry-point for this module is the `step1` function.
 import os
 import tempfile
 from jaxtyping import ArrayLike
-from numpy.typing import NDArray
 from typing import Optional
 from pandas import DataFrame
 from ._internal.level0 import level0

src/agricola/step2.py
@@ -78,7 +78,6 @@ class ParquetRotatingWriter:
         self.buffer_size = 0
         self.file_idx = 0
 
-        # TODO: document that output dir cannot exist
         Path.mkdir(output_dir, parents=True, exist_ok=False)
 
     def write(self, df):
@@ -457,7 +456,7 @@ def step2(
     if impute:
         M = jnp.ones(shape=jnp.asarray(Y).shape)
     else:
-        M = (~jnp.isnan(jnp.asarray(Y))).astype(jnp.float32)
+        M = (~jnp.isnan(jnp.asarray(Y))).astype(float)
 
     Y, X, step1_predictions_np, idx_sample, test_type_enum, trait_type_enum = (
         validate_step2_inputs(

commit aca05a62b01cecb015bc79b83823800e7d413d70

fix: correct error in calculating local ancestry dosages
Commit 886d92657884262ec2b951c65206d9a3ca99c960 introduced a bug by not
casting the ancestry masks to integers. When the haplotypes were summed
to get the ancestry dosage, this resulted in ancestry dosage
computed as the union of ancestry-deconvoluted haplotypes rather than the sum.

src/agricola/_internal/utils.py
@@ -104,8 +104,8 @@ def get_geno_lanc_deconv(
     geno = jnp.asarray(dataset.get_geno(indices))
     lanc = jnp.asarray(dataset.get_lanc(indices))
     ancestries = jnp.arange(len(dataset.ancestries))
-    left_haps_mask = lanc[:, :, 0:1] == ancestries[None, None, :]
-    right_haps_mask = lanc[:, :, 1:2] == ancestries[None, None, :]
+    left_haps_mask = (lanc[:, :, 0:1] == ancestries[None, None, :]).astype(int)
+    right_haps_mask = (lanc[:, :, 1:2] == ancestries[None, None, :]).astype(int)
     geno_masked = left_haps_mask * geno[:, :, 0:1] + right_haps_mask * geno[:, :, 1:2]
     lanc_masked = left_haps_mask + right_haps_mask
     return geno_masked, lanc_masked
@@ -126,7 +126,7 @@ def get_geno_deconv(dataset: LancData, indices: NDArray[np.uint32]) -> Array:
     geno = jnp.asarray(dataset.get_geno(indices))
     lanc = jnp.asarray(dataset.get_lanc(indices))
     ancestries = jnp.arange(len(dataset.ancestries))
-    left_haps_mask = lanc[:, :, 0:1] == ancestries[None, None, :]
-    right_haps_mask = lanc[:, :, 1:2] == ancestries[None, None, :]
+    left_haps_mask = (lanc[:, :, 0:1] == ancestries[None, None, :]).astype(int)
+    right_haps_mask = (lanc[:, :, 1:2] == ancestries[None, None, :]).astype(int)
     geno_masked = left_haps_mask * geno[:, :, 0:1] + right_haps_mask * geno[:, :, 1:2]
     return geno_masked